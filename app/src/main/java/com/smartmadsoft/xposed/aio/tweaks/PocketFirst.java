e253808bae66e1830f151d1db7be118e3ca256e0
Marcel Dopita <m@rcel.cz>
2015-12-31 18:24:32 +0100

    Fix "Pocket First" on Android 6.0

--- app/src/main/java/com/smartmadsoft/xposed/aio/tweaks/PocketFirst.java
+++ app/src/main/java/com/smartmadsoft/xposed/aio/tweaks/PocketFirst.java
@@ -1,5 +1,10 @@
 package com.smartmadsoft.xposed.aio.tweaks;
 
+import android.content.ComponentName;
+import android.os.Build;
+
+import java.util.LinkedHashMap;
+
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
@@ -8,13 +13,35 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class PocketFirst {
     public static void hook(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            XposedHelpers.findAndHookMethod("com.android.internal.app.ResolverActivity.ResolverComparator", lpparam.classLoader, "getPackageTimeSpent", String.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    if (param.args[0].equals("com.ideashower.readitlater.pro"))
-                        param.setResult(Long.MAX_VALUE);
-                }
-            });
+            if (Build.VERSION.SDK_INT >= 23) {
+                XposedBridge.hookAllMethods(XposedHelpers.findClass("com.android.internal.app.ResolverComparator", lpparam.classLoader), "compute", new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        boolean modded = false;
+                        @SuppressWarnings("unchecked")
+                        LinkedHashMap<ComponentName, Object> map = (LinkedHashMap<ComponentName, Object>) XposedHelpers.getObjectField(param.thisObject, "mScoredTargets");
+                        for (ComponentName key : map.keySet()) {
+                            if (key.getPackageName().equals("com.ideashower.readitlater.pro")) {
+                                Object target = map.get(key);
+                                XposedHelpers.setObjectField(target, "score", Float.MAX_VALUE);
+                                map.put(key, target);
+                                modded = true;
+                                break;
+                            }
+                        }
+                        if (modded)
+                            XposedHelpers.setObjectField(param.thisObject, "mScoredTargets", map);
+                    }
+                });
+            } else {
+                XposedHelpers.findAndHookMethod("com.android.internal.app.ResolverActivity.ResolverComparator", lpparam.classLoader, "getPackageTimeSpent", String.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        if (param.args[0].equals("com.ideashower.readitlater.pro"))
+                            param.setResult(Long.MAX_VALUE);
+                    }
+                });
+            }
         } catch (Throwable t) {
             XposedBridge.log(t);
         }
