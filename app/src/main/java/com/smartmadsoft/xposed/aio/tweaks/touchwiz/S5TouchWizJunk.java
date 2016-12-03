b164afbb272ca078eb5327554191c8b08f1c21e7
Marcel Dopita <m@rcel.cz>
2016-12-03 16:44:05 +0100

    TW BT/WiFi dialogs

--- app/src/main/java/com/smartmadsoft/xposed/aio/tweaks/touchwiz/S5TouchWizJunk.java
+++ app/src/main/java/com/smartmadsoft/xposed/aio/tweaks/touchwiz/S5TouchWizJunk.java
@@ -13,20 +13,22 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class S5TouchWizJunk {
     public static void hook(final XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            XposedBridge.hookAllMethods(XposedHelpers.findClass("com.sec.android.app.popupuireceiver.PopupuiReceiver", lpparam.classLoader), "showBatteryCoverPopup", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    param.setResult(null);
-                }
-            });
-
-            if (Build.VERSION.SDK_INT < 23) {
-                XposedHelpers.findAndHookMethod("com.sec.android.app.popupuireceiver.PopupuiService", lpparam.classLoader, "showUSBCDetacheddDialog", Context.class, new XC_MethodHook() {
+            if (Build.DEVICE.startsWith("klte")) {
+                XposedBridge.hookAllMethods(XposedHelpers.findClass("com.sec.android.app.popupuireceiver.PopupuiReceiver", lpparam.classLoader), "showBatteryCoverPopup", new XC_MethodHook() {
                     @Override
-                    protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                         param.setResult(null);
                     }
                 });
+
+                if (Build.VERSION.SDK_INT < 23) {
+                    XposedHelpers.findAndHookMethod("com.sec.android.app.popupuireceiver.PopupuiService", lpparam.classLoader, "showUSBCDetacheddDialog", Context.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                            param.setResult(null);
+                        }
+                    });
+                }
             }
         } catch (Throwable t) {
             XposedBridge.log(t);
@@ -35,20 +37,22 @@ public class S5TouchWizJunk {
 
     public static void hookUI(final XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "showHideQConnectLayout", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    param.setResult(null);
-                }
-            });
+            if (Build.DEVICE.startsWith("klte")) {
+                XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader, "showHideQConnectLayout", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        param.setResult(null);
+                    }
+                });
 
-            XposedHelpers.findAndHookMethod("com.android.systemui.qs.tiles.AirplaneModeTile", lpparam.classLoader, "showConfirmPopup", boolean.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    XposedHelpers.callMethod(param.thisObject, "setEnabled", param.args[0]);
-                    param.setResult(null);
-                }
-            });
+                XposedHelpers.findAndHookMethod("com.android.systemui.qs.tiles.AirplaneModeTile", lpparam.classLoader, "showConfirmPopup", boolean.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        XposedHelpers.callMethod(param.thisObject, "setEnabled", param.args[0]);
+                        param.setResult(null);
+                    }
+                });
+            }
         } catch (Throwable t) {
             XposedBridge.log(t);
         }
@@ -71,6 +75,17 @@ public class S5TouchWizJunk {
     public static void hookSettings(final XC_LoadPackage.LoadPackageParam lpparam) {
         try {
             XposedHelpers.findAndHookMethod("com.android.settings.bluetooth.BluetoothScanDialog", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    ((Activity)param.thisObject).finish();
+                }
+            });
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+        }
+
+        try {
+            XposedHelpers.findAndHookMethod("com.android.settings.wifi.WifiPickerDialog", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
                     ((Activity)param.thisObject).finish();
