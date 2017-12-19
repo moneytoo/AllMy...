9a0a420e80dbd4fab6305eb85707668b51800f9d
Marcel Dopita <m@rcel.cz>
2017-12-19 20:33:58 +0100

    Fix Maps

--- app/src/main/java/com/smartmadsoft/xposed/aio/tweaks/GMapsMinimizedBar.java
+++ app/src/main/java/com/smartmadsoft/xposed/aio/tweaks/GMapsMinimizedBar.java
@@ -8,45 +8,19 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class GMapsMinimizedBar {
-    private static boolean firstHit = false;
-
     public static void hook(final XC_LoadPackage.LoadPackageParam lpparam) {
         try {
             XposedHelpers.findAndHookMethod(XposedHelpers.findClass("com.google.android.apps.gmm.base.app.GoogleMapsApplication", lpparam.classLoader), "attachBaseContext", Context.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-
-                    // <= 9.66.1
-                    try {
-                        XposedHelpers.findAndHookMethod("com.google.android.apps.gmm.home.views.HomeBottomSheetView", lpparam.classLoader, "c", new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                XposedHelpers.setObjectField(param.thisObject, "b", 0.0f);
-                            }
-                        });
-                    } catch (Throwable t) {}
-
-                    // >= 9.67.1
-                    try {
-                        XposedHelpers.findAndHookMethod("com.google.android.apps.gmm.home.views.HomeBottomSheetView", lpparam.classLoader, "b", int.class, boolean.class, new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                if (firstHit) {
-                                    param.args[0] = 0;
-                                    firstHit = false;
-                                }
-                            }
-                        });
-                    } catch (Throwable t) {
-                        XposedBridge.log(t);
-                    }
-
-
+                    // >= 9.68.2
                     try {
                         XposedBridge.hookAllConstructors(XposedHelpers.findClass("com.google.android.apps.gmm.home.views.HomeBottomSheetView", lpparam.classLoader), new XC_MethodHook() {
                             @Override
                             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                firstHit = true;
+                                // Black magic
+                                XposedHelpers.setObjectField(param.thisObject, "l", 0);
+                                XposedHelpers.setObjectField(param.thisObject, "m", 0);
                             }
                         });
                     } catch (Throwable t) {
